d8a44ef4802f98de0872e211402e92de216a65a7
tinodin <constantin.schlueter@icloud.com>
2025-06-05 14:49:28 +0200

    Bump version

--- Views/Settings/GamesPage.xaml.cs
+++ Views/Settings/GamesPage.xaml.cs
@@ -4,7 +4,6 @@ using Microsoft.UI.Xaml.Media.Imaging;
 using System.Diagnostics;
 using System.Text.Json;
 using Windows.Storage;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AutoOS.Views.Settings;
 
