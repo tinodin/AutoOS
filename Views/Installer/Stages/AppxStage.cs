c0a8721fea1eb4b0ecec65cfdf5c54e78c58c793
tinodin <constantin.schlueter@icloud.com>
2025-06-09 13:59:54 +0200

    Also uninstall Microsoft.Edge.GameAssist_8wekyb3d8bbwe

--- Views/Installer/Stages/AppxStage.cs
+++ Views/Installer/Stages/AppxStage.cs
@@ -36,7 +36,7 @@ public static class AppxStage
             ("Uninstalling provisioned Microsoft.BingNews_8wekyb3d8bbwe", async () => await ProcessActions.RemoveAppxProvisioned("Microsoft.BingNews"), null),
             ("Deprovisioning Microsoft.BingNews_8wekyb3d8bbwe", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"reg add ""HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\Microsoft.BingNews_8wekyb3d8bbwe"" /f"), null),
 
-            // bing news
+            // bing search
             //("Uninstalling Microsoft.BingSearch_8wekyb3d8bbwe", async () => await ProcessActions.RemoveAppx("Microsoft.BingSearch"), null),
             //("Uninstalling provisioned Microsoft.BingSearch_8wekyb3d8bbwe", async () => await ProcessActions.RemoveAppxProvisioned("Microsoft.BingSearch"), null),
             //("Deprovisioning Microsoft.BingSearch_8wekyb3d8bbwe", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"reg add ""HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\Microsoft.BingSearch"" /f"), null),
@@ -45,6 +45,11 @@ public static class AppxStage
             ("Uninstalling Microsoft.BingWeather_8wekyb3d8bbwe", async () => await ProcessActions.RemoveAppx("Microsoft.BingWeather"), null),
             ("Uninstalling provisioned Microsoft.BingWeather_8wekyb3d8bbwe", async () => await ProcessActions.RemoveAppxProvisioned("Microsoft.BingWeather"), null),
             ("Deprovisioning Microsoft.BingWeather_8wekyb3d8bbwe", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"reg add ""HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\Microsoft.BingWeather_8wekyb3d8bbwe"" /f"), null),
+
+            // edge game assist
+            ("Uninstalling Microsoft.Edge.GameAssist_8wekyb3d8bbwe", async () => await ProcessActions.RemoveAppx("Microsoft.Edge.GameAssist"), null),
+            ("Uninstalling provisioned Microsoft.Edge.GameAssist_8wekyb3d8bbwe", async () => await ProcessActions.RemoveAppxProvisioned("Microsoft.Edge.GameAssist"), null),
+            ("Deprovisioning Microsoft.Edge.GameAssist_8wekyb3d8bbwe", async () => await ProcessActions.RunNsudo("TrustedInstaller", @"reg add ""HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\Microsoft.Edge.GameAssist_8wekyb3d8bbwe"" /f"), null),
 
             // copilot
             ("Uninstalling Microsoft.Copilot_8wekyb3d8bbwe", async () => await ProcessActions.RemoveAppx("Microsoft.Copilot"), null),
