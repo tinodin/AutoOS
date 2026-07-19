d13c4f37e5ca65238b5f9a4ea62409b01f728d03
tinodin <constantin.schlueter@icloud.com>
2026-07-19 15:51:55 +0200

    Support Legacy AMD Drivers

--- src/AutoOS.Core/Helpers/GPU/AmdHelper.cs
+++ src/AutoOS.Core/Helpers/GPU/AmdHelper.cs
@@ -22,12 +22,13 @@ public static partial class AmdHelper
 
 		var endpoints = new[]
 		{
-			"https://www2.ati.com/drivers/installer/json/DrvDldDetails_Consumer_Legacy_Win10.json",
-			"https://www2.ati.com/drivers/installer/json/DrvDldDetails_Consumer_WHQL_Win11.json",
-			"https://www2.ati.com/drivers/installer/json/DrvDldDetails_Consumer_WHQL_Win11_p.json",
-			"https://www2.ati.com/drivers/installer/json/drvdlddetails_ws_maintenance_win11.json",
-			"https://www2.ati.com/drivers/installer/json/DrvDldDetails_WS_Legacy_Win10.json"
-		};
+		"https://www2.ati.com/drivers/installer/json/DrvDldDetails_Consumer_Legacy_Win10.json",
+		"https://www2.ati.com/drivers/installer/json/DrvDldDetails_Consumer_WHQL_Win11.json",
+		"https://www2.ati.com/drivers/installer/json/DrvDldDetails_Consumer_WHQL_Win11_p.json",
+		"https://www2.ati.com/drivers/installer/json/drvdlddetails_ws_maintenance_win11.json",
+		"https://www2.ati.com/drivers/installer/json/DrvDldDetails_WS_Legacy_Win10.json",
+		"https://www2.ati.com/drivers/installer/json/DrvDldDetails_Consumer_WHQL_Win7.json"
+	};
 
 		httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Referer", "http://support.amd.com");
 		httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "*/*");
@@ -38,8 +39,11 @@ public static partial class AmdHelper
 		var jsonTasks = endpoints.Select(endpoint => httpClient.GetStringAsync(endpoint));
 		var responses = await Task.WhenAll(jsonTasks);
 
-		foreach (var json in responses)
+		for (int i = 0; i < responses.Length; i++)
 		{
+			var json = responses[i];
+			var currentEndpoint = endpoints[i];
+
 			var builds = JsonNode.Parse(json).AsArray();
 			if (builds == null) continue;
 
@@ -57,7 +61,16 @@ public static partial class AmdHelper
 						continue;
 
 					newestVersion = string.Join(".", build["externalbuildversion"].ToString().Split('.').Take(3));
-					newestDownloadUrl = build["fullbuild"]?.ToString();
+
+					if (currentEndpoint.EndsWith("DrvDldDetails_Consumer_WHQL_Win7.json", StringComparison.OrdinalIgnoreCase))
+					{
+						newestDownloadUrl = "https://www2.ati.com/drivers/radeon-software-adrenalin-2020-22.6.1-win10-win11-64bit-legacyasics-june23-2022-legacy.exe";
+					}
+					else
+					{
+						newestDownloadUrl = build["fullbuild"]?.ToString();
+					}
+
 					break;
 				}
 
